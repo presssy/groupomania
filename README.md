ff44e391548de9250fedf4436390aa8dcd4c3774
Benoit Garcia <bgarcia@kaliop.com>
2023-02-20 15:59:45 +0100

    init: node express stack

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# groupomania
+# Groupomania
+
 OpenClassrooms Project
---
+++ api/src/index.ts
@@ -0,0 +1,31 @@
+import express, {Application} from "express"
+import cors from "cors"
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+class App {
+    public app: Application
+
+    constructor() {
+        this.app = express()
+        this.config()
+
+        this.app.listen(process.env.API_PORT || 4040, () => {
+            console.log(`🚀 Server is launching...`);
+            console.log(
+                `Server is ready on ${process.env.API_ENV || "http://localhost"}:${process.env.API_PORT || 4040}`
+            );
+        })
+    }
+
+    private config(): void {
+        this.app.use(cors({
+            origin: "http://localhost:5173",
+            credentials: true
+        }))
+        this.app.use(express.json())
+    }
+}
+
+export default new App().app
