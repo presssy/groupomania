8d487525a9264dfb8d83cf00120a8dff6db7d5b2
presssy <bgarcia@kaliop.com>
2023-02-21 06:25:18 +0100

    add: update user register controller

--- api/src/controllers/users.ts
+++ api/src/controllers/users.ts
@@ -1,10 +1,13 @@
 import Controllers from "@/controllers/index";
 import {Request, Response} from "express";
+import bcrypt from "bcrypt"
 
 class UsersController extends Controllers{
 
     constructor() {
         super()
+
+        this.registerUser = this.registerUser.bind(this)
     }
 
     public async registerUser(req: Request, res: Response): Promise<Response> {
@@ -13,7 +16,16 @@ class UsersController extends Controllers{
             const userAlreadyExist = await this.services.prisma.user.findUnique({where: {email}})
             if(userAlreadyExist) throw this.customError("User already registered", 401)
 
-            return this.success(res, 201, userAlreadyExist)
+            const hashPassword = await bcrypt.hash(password, 10)
+
+            const registeringUser = await this.services.prisma.user.create({
+                data: {
+                    email,
+                    password: hashPassword
+                }
+            })
+
+            return this.success(res, 201, registeringUser)
         }
         catch(err: any) {
             return this.error(res, err.code, err.message)
